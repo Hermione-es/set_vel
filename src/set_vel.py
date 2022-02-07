ea3cbacb02de1dcab156b28c1c48d3bd06f9d732
Hermione-es <78470447+Hermione-es@users.noreply.github.com>
2022-02-07 17:56:51 +0900

    Update set_vel.py

--- src/set_vel.py
+++ src/set_vel.py
@@ -11,29 +11,46 @@ def time_vel(start_time):
     duration = time.time() - start_time
     rospy.loginfo(duration)
 
-    if duration < 5:
-        control_linear_vel = 0.2
+    if duration < 10:
+        control_linear_vel = 0
         control_angular_vel = 0
 
-    elif duration > 5 and duration < 15:
-        control_linear_vel = -0.2
+    elif duration < 40:
+        control_linear_vel = 0.03
         control_angular_vel = 0
 
-    elif duration > 15 and duration < 25:
-        control_linear_vel = 0.2
+    elif duration >= 40 and duration < 100:
+        control_linear_vel = -0.03
         control_angular_vel = 0
-        
+
+    elif duration >= 100 and duration < 160:
+        control_linear_vel = 0.03
+        control_angular_vel = 0
+
+    elif duration >= 160 and duration < 220:
+        control_linear_vel = -0.03
+        control_angular_vel = 0
+
+    elif duration >= 220 and duration < 280:
+        control_linear_vel = 0.03
+        control_angular_vel = 0
+
+    elif duration >= 280 and duration < 310:
+        control_linear_vel = -0.03
+        control_angular_vel = 0
+
     else :
         control_linear_vel = 0
         control_angular_vel = 0
-    
-    return control_linear_vel, control_angular_vel 
+
+    return control_linear_vel, control_angular_vel
+
 
 
 if __name__=="__main__":
-    
+
     rospy.init_node('set_vel')
-    
+
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
     twist = Twist()
@@ -43,7 +60,7 @@ if __name__=="__main__":
 
     while not rospy.is_shutdown():
         linear_vel, angular_vel = time_vel(start_time)
-        
+
         twist.linear.x = linear_vel
         twist.linear.y = 0.0
         twist.linear.z = 0.0
@@ -53,5 +70,5 @@ if __name__=="__main__":
         pub.publish(twist)
 
         rate.sleep()
-    
+
     rospy.spin()
