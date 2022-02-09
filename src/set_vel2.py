9f3306506eacd4e21352bf3276b7623dc2bf4199
Hermione-es <bes3720@gmail.com>
2022-02-09 13:13:22 +0900

    commit

--- src/set_vel2.py
+++ src/set_vel2.py
@@ -1,45 +1,44 @@
 #!/usr/bin/env python
 #rotation drive
 import rospy
-import math
-
 import time
 from geometry_msgs.msg import Twist
 
 
 def time_vel(start_time):
+    
+    current_time =  rospy.Time.now()
+    dt = current_time - start_time
 
-    duration = time.time() - start_time
-
-    if duration < 10:
+    if dt.to_sec() < 10:
         control_linear_vel = 0
         control_angular_vel = 0
 
-    elif duration >= 10 and duration < 80:
+    elif dt.to_sec() >= 10 and dt.to_sec() < 80:
         control_linear_vel = 0.0448798951 # radius*2*pi/time
         control_angular_vel = 0.0897597901 # 2*pi/time
 
-    elif duration >= 80 and duration < 85:
+    elif dt.to_sec() >= 80 and dt.to_sec() < 85:
         control_linear_vel = 0
         control_angular_vel = 0
 
-    elif duration >= 85 and duration < 155:
+    elif dt.to_sec() >= 85 and dt.to_sec() < 155:
         control_linear_vel = 0.0448798951 # radius*2*pi/time
         control_angular_vel = -0.0897597901 # 2*pi/time
 
-    elif duration >= 155 and duration <160:
+    elif dt.to_sec() >= 155 and dt.to_sec() <160:
         control_linear_vel = 0
         control_angular_vel = 0
 
-    elif duration >= 160 and duration < 230:
+    elif dt.to_sec() >= 160 and dt.to_sec() < 230:
         control_linear_vel = 0.0448798951 # radius*2*pi/time
         control_angular_vel = 0.0897597901 # 2*pi/time
 
-    elif duration >= 230 and duration < 235:
+    elif dt.to_sec() >= 230 and dt.to_sec() < 235:
         control_linear_vel = 0
         control_angular_vel = 0
 
-    elif duration >= 235 and duration < 305:
+    elif dt.to_sec() >= 235 and dt.to_sec() < 305:
         control_linear_vel = 0.0448798951 # radius*2*pi/time
         control_angular_vel = -0.0897597901 # 2*pi/time
 
@@ -55,12 +54,12 @@ if __name__=="__main__":
 
     rospy.init_node('set_vel')
 
-    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=3)
 
     twist = Twist()
 
-    start_time = time.time()
-    rate = rospy.Rate(10) # 10hz
+    start_time =  rospy.Time.now()
+    rate = rospy.Rate(50) # 10hz
 
     while not rospy.is_shutdown():
         linear_vel, angular_vel = time_vel(start_time)
