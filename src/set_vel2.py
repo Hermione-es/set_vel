d723baac9e7b804b38c0bdaa527e84a5bcb77427
Hermione-es <bes3720@gmail.com>
2022-02-28 14:27:35 +0900

    commit

--- src/set_vel2.py
+++ src/set_vel2.py
@@ -15,32 +15,32 @@ def time_vel(start_time):
         control_angular_vel = 0
 
     elif dt.to_sec() >= 10 and dt.to_sec() < 80:
-        control_linear_vel = 0.04487989505  # radius*2*pi/time
-        control_angular_vel = 0.09487889505 # 2*pi/time
+        control_linear_vel = 0.0448798950513  # radius*2*pi/time
+        control_angular_vel = 0.089579790102566 # 2*pi/time
 
     elif dt.to_sec() >= 80 and dt.to_sec() < 95:
         control_linear_vel = 0
         control_angular_vel = 0
 
     elif dt.to_sec() >= 95 and dt.to_sec() < 165:
-        control_linear_vel = 0.04487989505  # radius*2*pi/time
-        control_angular_vel = -0.09487889505 # 2*pi/time
+        control_linear_vel = 0.0448798950513  # radius*2*pi/time
+        control_angular_vel = -0.089579790102566 # 2*pi/time
 
     elif dt.to_sec() >= 165 and dt.to_sec() <170:
         control_linear_vel = 0
         control_angular_vel = 0
 
     elif dt.to_sec() >= 170 and dt.to_sec() < 240:
-        control_linear_vel = 0.04487989505  # radius*2*pi/time
-        control_angular_vel = 0.09487889505 # 2*pi/time
+        control_linear_vel = 0.0448798950513  # radius*2*pi/time
+        control_angular_vel = 0.089579790102566 # 2*pi/time
 
     elif dt.to_sec() >= 240 and dt.to_sec() < 245:
         control_linear_vel = 0
         control_angular_vel = 0
 
     elif dt.to_sec() >= 245 and dt.to_sec() < 315:
-        control_linear_vel = 0.04487989505  # radius*2*pi/time
-        control_angular_vel = -0.09487889505 # 2*pi/time
+        control_linear_vel = 0.0448798950513  # radius*2*pi/time
+        control_angular_vel = -0.089579790102566 # 2*pi/time
 
     else :
         control_linear_vel = 0
